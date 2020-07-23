494cdc2c3f9ce06062e750ea2f02aa50ad56a9f8
ravi7648 <ravishankar123yadav@gmail.com>
2020-07-24 02:06:43 +0530

    added makefile

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,14 +4,14 @@ function guess(){
     ans=$(ls -l |grep "^-"|wc -l)
     while true;
     do
-        echo "pleas enter your guess"
+        echo "please, enter your guess."
         read  num
         if [ $num -lt $ans ]
         then
-            echo "your guess is Less then the true number"
+            echo "your guess is Less then the true number."
         elif [ $num -gt $ans ]
         then
-            echo "your guess is Greater then the true number"
+            echo "your guess is Greater then the true number."
         else
             echo "Congratulation! you are right."
         break;
